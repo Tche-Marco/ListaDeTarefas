bf85e5b061979a0aa0235c9c04363627eadc98f8
Marco Antônio <marcomartins06@rede.ulbra.br>
2023-04-21 00:24:56 -0300

    Adicionando itens na lista

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:path_provider/path_provider.dart';
 void main() {
   runApp(const MaterialApp(
     home: Home(),
+    debugShowCheckedModeBanner: false,
   ));
 }
 
@@ -18,11 +19,84 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final _toDoController = TextEditingController();
   List _toDoList = [];
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = {};
+      newToDo["title"] = _toDoController.text;
+      newToDo["ok"] = false;
+      _toDoController.clear();
+      _toDoList.add(newToDo);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de tarefas"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _toDoController,
+                    decoration: const InputDecoration(
+                      labelText: "Nova Tarefa",
+                      labelStyle: TextStyle(
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _addToDo,
+                  style: ElevatedButton.styleFrom(
+                    textStyle: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    backgroundColor: Colors.blueAccent,
+                  ),
+                  child: const Text("ADD"),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.only(
+                top: 10,
+              ),
+              itemCount: _toDoList.length,
+              itemBuilder: (context, index) {
+                return CheckboxListTile(
+                  onChanged: (value) {
+                    setState(() {
+                      _toDoList[index]["ok"] = value;
+                    });
+                  },
+                  title: Text(_toDoList[index]["title"]),
+                  value: _toDoList[index]["ok"],
+                  secondary: CircleAvatar(
+                    child: Icon(
+                      _toDoList[index]["ok"] ? Icons.check_box : Icons.error,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Future<File> _getFile() async {
